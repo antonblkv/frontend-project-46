1f064bc680f3fefbed550d2270ef8128857426aa
Anton <antonblkv@yandex.ru>
2024-05-23 14:45:37 +0300

    fix step 5

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -1,17 +1,15 @@
-import { dirname } from 'path';
+import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
-import genDiff from '../src/genDiff';
-import path from 'path';
+import genDiff from '../src/genDiff.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const result =
-	'- follow: false\n  host: hexlet.io\n- proxy: 123.234.53.22\n- timeout: 50\n+ timeout: 20\n+ verbose: true\n';
+const result = '- follow: false\n  host: hexlet.io\n- proxy: 123.234.53.22\n- timeout: 50\n+ timeout: 20\n+ verbose: true\n';
 
 test('genDiff', () => {
-	const filePath1 = path.join(__dirname, '..', '__fixtures__', 'file1.json');
-	const filePath2 = path.join(__dirname, '..', '__fixtures__', 'file2.json');
+  const filePath1 = path.join(__dirname, '..', '__fixtures__', 'file1.json');
+  const filePath2 = path.join(__dirname, '..', '__fixtures__', 'file2.json');
 
-	expect(genDiff(filePath1, filePath2)).toEqual(result);
+  expect(genDiff(filePath1, filePath2)).toEqual(result);
 });
--- src/genDiff.js
+++ src/genDiff.js
@@ -1,24 +1,24 @@
-import parse from './parse.js';
 import _ from 'lodash';
-
+import parse from './parse.js';
 
 const genDiff = (file1, file2) => {
-	const data1 = parse(file1);
-	const data2 = parse(file2);
-	const keys = [..._.keys(data1), ..._.keys(data2)];
-	const sortedKeys = _.sortedUniq(_.sortBy(keys));
-	let result = '';
+  const data1 = parse(file1);
+  const data2 = parse(file2);
+  const keys = [..._.keys(data1), ..._.keys(data2)];
+  const sortedKeys = _.sortedUniq(_.sortBy(keys));
+  let result = '';
 
+  /* eslint-disable-next-line */
 	for (const key of sortedKeys) {
-		if (data1[key] === data2[key]) {
-			result += `  ${key}: ${data1[key]}\n`;
-		} else {
-			result += data1.hasOwnProperty(key) ? `- ${key}: ${data1[key]}\n` : '';
-			result += data2.hasOwnProperty(key) ? `+ ${key}: ${data2[key]}\n` : '';
-		}
-	}
+    if (data1[key] === data2[key]) {
+      result += `  ${key}: ${data1[key]}\n`;
+    } else {
+      result += Object.hasOwn(data1, key) ? `- ${key}: ${data1[key]}\n` : '';
+      result += Object.hasOwn(data2, key) ? `+ ${key}: ${data2[key]}\n` : '';
+    }
+  }
 
-	return result;
+  return result;
 };
 
 export default genDiff;

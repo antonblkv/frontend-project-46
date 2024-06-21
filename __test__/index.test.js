417b2dafe556dfff6967c795ea20dcd8a5ff3362
Belyakov Anton <antonblkv@yandex.ru>
2024-06-21 22:04:49 +0300

    draft 7 step

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -1,11 +1,12 @@
 import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import genDiff from '../src/genDiff.js';
+import fs from 'fs';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const result = '- follow: false\n  host: hexlet.io\n- proxy: 123.234.53.22\n- timeout: 50\n+ timeout: 20\n+ verbose: true';
+	const result = fs.readFileSync(path.join(__dirname, '..', '__fixtures__', 'result.txt'), 'utf-8');
 
 test('genDiffExtensionJSON', () => {
 	const filePath1 = path.join(__dirname, '..', '__fixtures__', 'file1.json');
--- bin/gendiff.js
+++ bin/gendiff.js
@@ -10,9 +10,9 @@ program
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1')
   .arguments('<filepath1> <filepath2>')
-  .option('-f, --format [type]', 'output format')
+  .option('-f, --format <type>', 'output format (default: "stylish")', 'stylish')
   .action((filepath1, filepath2) => {
-    genDiff((filepath1), (filepath2));
+    console.log(genDiff(filepath1, filepath2, program.opts().format));
   });
 
 program.parse();
